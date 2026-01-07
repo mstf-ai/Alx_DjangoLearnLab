ca751eafc448d4d20d207e0d8118505035193a14
mstf-ai <albrnsmstfy252@gmail.com>
2026-01-07 08:49:41 +0200

    new commit

--- django-models/relationship_app/templates/relationship_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Book
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        # اختر الحقول التي تريد أن يظهرها في النموذج
-        fields = ["title", "author", "publication_year", "library"]
