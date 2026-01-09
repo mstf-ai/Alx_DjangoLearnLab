4193c11315df8c61ddd00a344ba4905a601f73ad
mstf-ai <albrnsmstfy252@gmail.com>
2026-01-09 21:52:33 +0200

    new commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,9 +4,9 @@ from .models import Book
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_books(request):
+def book_list(request):
     books = Book.objects.all()
-    return render(request, 'view_books.html', {'books': books})
+    return render(request, 'book_list.html', {'books': books})
 
 
 @permission_required('bookshelf.can_create', raise_exception=True)
